af4bc96e7f53639963ca93bac7e8746a33bfebb4
carlos-osorio-developer <osorio.uis@gmail.com>
2021-10-27 17:17:07 -0500

    Add function to calculate new price of the house

--- tda.cpp
+++ tda.cpp
@@ -8,6 +8,44 @@ struct cliente {
   int valorVivienda;
 };
 
+int calculadora(int valorPredio, int tipoVivienda) {
+
+  int criterio;
+  int valorNuevo;
+
+  if(tipoVivienda==1) {
+    cout << "\n" << "Ya que su predio se encuentra en obra negra, ingrese su dimension en metros cuadrados: "; 
+    while (!(cin >> criterio)){   
+      cout << "\n" << "Error => Ingrese unicamente numeros: "; 
+      cin.clear();
+      cin.ignore(123, '\n');    
+    }
+    
+    if(criterio<60) {
+      valorNuevo = valorPredio*0.9;
+    }
+    else {
+      valorNuevo = valorPredio*0.85;
+    }
+  }
+  else {
+    cout << "\n" << "Ya que su predio tiene acabados, ingrese el nivel de estrato: "; 
+    while (!(cin >> criterio)){   
+      cout << "\n" << "Error => Ingrese unicamente numeros: "; 
+      cin.clear();
+      cin.ignore(123, '\n');    
+    }
+
+    if(criterio>4) {
+      valorNuevo = valorPredio*1.2;
+    }
+    else {
+      valorNuevo = valorPredio*1.1;
+    }
+  }  
+  return valorNuevo;
+}
+
 int main() {
 
   cliente user;
@@ -36,5 +74,8 @@ int main() {
     cin >> user.tipoVivienda;   
     cin.clear();
     cin.ignore(123, '\n');           
-  }  
+  }
+
+  int valorizacion;
+  valorizacion = calculadora(user.valorVivienda, user.tipoVivienda);
 }
